4cc191ff21081fcdc5e116c0acff65057509da4f
Luis Flavio <luisflaviomont@gmail.com>
2024-07-26 22:27:42 -0300

    1.09.12 - Delete Roles

--- Memorial3/Controllers/AdminController.cs
+++ Memorial3/Controllers/AdminController.cs
@@ -177,5 +177,40 @@ namespace Memorial3.Controllers
 
             return RedirectToAction("EditRole", new { Id = id });
         }
+
+        [HttpGet]
+        public async Task<ActionResult> DeleteRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            return View(role);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> ConfirmDelete(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewData["ErrorMessage"] = $"No role with Id '{id}' was found";
+                return View("Error");
+            }
+            else
+            {
+                var result = await _roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListAllRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+
+                return View(role);
+            }
+        }
     }
 }
