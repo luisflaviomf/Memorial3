68da21891fcd6cf97abe42973919cbfc03fb2dfe
Luis Flavio Ferreira Monteiro <luisflaviomont@gmail.com>
2024-08-05 19:21:37 -0300

    1.16 - Some fixes on buttons

---
+++ Memorial3/ViewModel/MemorialDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Memorial3.Models;
+
+namespace Memorial3.ViewModels
+{
+    public class MemorialDetailsViewModel
+    {
+        public Memorial Memorial { get; set; }
+        public bool CanEdit { get; set; }
+    }
+}
